df5dbaa310ea4edc11a4bfc328a63d7da07ddfd9
innlouvate <l.j.franklin85@googlemail.com>
2017-07-23 23:21:08 +0100

    allow enter to submit login

--- src/application/Login.java
+++ src/application/Login.java
@@ -3,6 +3,8 @@ package application;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 
 public class Login {
@@ -29,6 +31,31 @@ public class Login {
                 } catch (IOException e1) {
                     e1.printStackTrace();
                 }
+            }
+        });
+
+        loginName.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    try {
+                        ChatClient client = new ChatClient(loginName.getText());
+                        login.setVisible(false);
+                        login.dispose();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
             }
         });
     }
